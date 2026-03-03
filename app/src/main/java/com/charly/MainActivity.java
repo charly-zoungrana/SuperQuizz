42c7acf8fe7c5676423d4cf6a26af46efd75d512
Lyrach2004 <charlyzoungrana2004@gmail.com>
2026-03-03 15:07:22 +0000

    amélioration du fragment d'accueil-ajout du fragement dans l'actitivité principale-Ecoute des actions de l'utilisateur

--- app/src/main/java/com/charly/MainActivity.java
+++ app/src/main/java/com/charly/MainActivity.java
@@ -3,6 +3,7 @@
 package com.charly;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +11,20 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.charly.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/charly/WelcomeFragment.java
+++ app/src/main/java/com/charly/WelcomeFragment.java
@@ -1,14 +1,19 @@
 package com.charly;
-
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.charly.databinding.FragmentWelcomeBinding;
+
 public class WelcomeFragment extends Fragment {
+
+    private FragmentWelcomeBinding binding;
     public static WelcomeFragment newInstance() {
         WelcomeFragment fragment = new WelcomeFragment();
         return fragment;
@@ -23,6 +28,44 @@ public class WelcomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false);
+        binding=FragmentWelcomeBinding.inflate(inflater,container,false);
+        return binding.getRoot();
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        binding.playButton.setEnabled(false);
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.nameInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {
+                binding.playButton.setEnabled(!s.toString().isEmpty());
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+        });
+
+        binding.playButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+                Log.d("Charly","Click!");
+            }
+        });
     }
 }
